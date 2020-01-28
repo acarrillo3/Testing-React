2215654df154de6cee00e095a569c942695636de
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-27 21:00:24 -0500

    previous button can be cliked and test test passed

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -7,28 +7,30 @@ import StarWarsCharacters from './StarWarsCharacters';
 
 // button click tests
 test("Previous button can be clicked", () => {
-    const {queryByText} = render(<StarWarsCharacters />);
-    const previousButton = queryByText(/previous/i);
-    fireEvent.click(previousButton);
+     const {queryByText} = render(<StarWarsCharacters />);
+     const previousButton = queryByText(/previous/i);
+     fireEvent.click(previousButton);
 });
 
-// test("Next Renders New Data", async () => {
-//     mockGetData.mockResolvedValue(initialData);
+//  test("Next Renders New Data", async () => {
+//      mockGetData.mockResolvedValue(initialData);
     
-//     const { getByText } = render(<StarWarsCharacters />);
-//     const previousButton = getByText(/Previous/i);
-//     const nextButton = getByText(/Next/i);
-//     mockGetData.mockResolvedValue(nextData);
-//     await wait(() => fireEvent.click(nextButton));
-//     await wait(() => expect(getByText("NEXT_NAME")));
-//   });
-//   test("Previous Renders New Data", async () => {
-//     mockGetData.mockResolvedValue(initialData);
+//      const { getByText } = render(<StarWarsCharacters />);
+//      const previousButton = getByText(/Previous/i);
+//      const nextButton = getByText(/Next/i);
+//      mockGetData.mockResolvedValue(nextData);
+//      await wait(() => fireEvent.click(nextButton));
+//      await wait(() => expect(getByText("NEXT_NAME")));
+//    });
+//    test("Previous Renders New Data", async () => {
+//      mockGetData.mockResolvedValue(initialData);
     
-//     const { getByText } = render(<StarWarsCharacters />);
-//     const previousButton = getByText(/Previous/i);
-//     const nextButton = getByText(/Next/i);
-//     mockGetData.mockResolvedValue(prevData);
-//     await wait(() => fireEvent.click(previousButton));
-//     await wait(() => expect(getByText("PREVIOUS_NAME")));
-//   });
+//      const { getByText } = render(<StarWarsCharacters />);
+//      const previousButton = getByText(/Previous/i);
+//      const nextButton = getByText(/Next/i);
+//      mockGetData.mockResolvedValue(prevData);
+//      await wait(() => fireEvent.click(previousButton));
+//      await wait(() => expect(getByText("PREVIOUS_NAME")));
+//    });
+
+    test('test test', () => {});
